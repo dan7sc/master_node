cc8ae179421df66d66954ae4dd0aa7a6a97fb48d
daniel sc <su7cheng@gmail.com>
2019-08-28 11:09:51 -0300

    Section 3 - Logging To Console: Add debug logs (if NODE_DEBUG=workers).

---
+++ section3/logging-to-console/lib/helpers.js
@@ -0,0 +1,118 @@
+/*
+ * Helpers for various tasks
+ *
+ */
+
+// Dependencies
+const config = require('../config');
+const crypto = require('crypto');
+const https = require('https');
+const querystring = require('querystring');
+
+// Container for all the helpers
+const helpers = {};
+
+// Parse a JSON string for an object in all cases, without throwing
+helpers.parseJsonToObject = (str) => {
+    try {
+        const obj = JSON.parse(str);
+        return obj;
+    } catch(e) {
+        return {};
+    }
+};
+
+// Create a SHA256 hash
+helpers.hash = (str) => {
+    if (typeof(str) === 'string' && str.length > 0) {
+        const hash = crypto.createHmac('sha256', config.hashingSecret)
+            .update(str).digest('hex');
+        return hash;
+    } else {
+        return false;
+    }
+};
+
+// Create a string of random alphanumeric characters, of a given length
+helpers.createRandomString = (strLength) => {
+    strLength = typeof(strLength) === 'number' && strLength > 0 ?
+        strLength : false;
+    if (strLength) {
+        // Define all possible characters that could go into a string
+        const possibleCharacters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+        // Start the final string
+        let str ='';
+        for (let i = 1; i <= strLength; i++) {
+            // Get a random character from the possibleCharacters string
+            const randomCharacter = possibleCharacters.charAt(
+                Math.floor(Math.random() * possibleCharacters.length)
+            ); 
+            str += randomCharacter;
+        }
+        // Return the final string
+        return str;
+    } else {
+        return false;
+    }
+};
+
+helpers.sendTwilioSms = (phone, msg, callback) => {
+    // Validate parameters
+    phone = typeof(phone) === 'string' && phone.trim().length === 10 ?
+        phone.trim() : false;
+    msg = typeof(msg) === 'string' &&
+        msg.trim().length > 0 && msg.trim().length <= 1600 ?
+        msg.trim() : false;
+    if (phone && msg) {
+
+        // Configure the request payload
+        const payload = {
+            'From': config.twilio.fromPhone,
+            'To': '+1' + phone,
+            'Body': msg
+        };
+        const stringPayload = querystring.stringify(payload);
+
+        // Configure the request details
+        const requestDetails = {
+            'protocol': 'https:',
+            'hostname': 'api.twilio.com',
+            'method': 'POST',
+            'path': '/2010-04-01/Accounts' + config.twilio.accountSid + '/Messages.json',
+            'auth': config.twilio.accountSid + ':' + config.twilio.authToken,
+            'headers': {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(stringPayload)
+            }
+        };
+
+        // Instantiate the request object
+        const req = https.request(requestDetails, (res) => {
+            // Grab the status of the sent request
+            const status = res.statusCode;
+            // Callback successfully if the request went through
+            if (status === 200 || status === 201) {
+                callback(false);
+            } else {
+                callback('Status code returned was ' + status);
+            }
+        });
+
+        // Bind to the error event so it doesn't get thrown
+        req.on('error', (e) => {
+            callback(e);
+        });
+
+        // Add the payload
+        req.write(stringPayload);
+
+        // End the request
+        req.end();
+    } else {
+        callback('Given parameters were missing or invalid');
+    }
+};
+
+// Export module
+module.exports = helpers;
---
+++ section3/logging-to-console/lib/server.js
@@ -0,0 +1,132 @@
+/*
+ * Server-related tasks
+ *
+ */
+
+// Dependencies
+const http = require('http');
+const https = require('https');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
+const config = require('./../config');
+const fs = require('fs');
+const handlers = require('./handlers');
+const helpers = require('./helpers');
+const path = require('path');
+const util = require('util');
+const debug = util.debuglog('workers');
+
+// Instantiate the server module object
+const server = {};
+
+// Instantiate the HTTP server
+server.httpServer = http.createServer((req, res) => {
+    server.unifiedServer(req, res);
+});
+
+// Instantiate the HTTPS server
+server.httpsServerOptions = {
+    'key': fs.readFileSync(path.join(__dirname, '/../https/key.pem')),
+    'cert': fs.readFileSync(path.join(__dirname, '/../https/cert.pem'))
+};
+
+server.httpsServer = https.createServer(server.httpsServerOptions, (req, res) => {
+    server.unifiedServer(req, res);
+});
+
+// Configure the server to respond to all requests with a string
+server.unifiedServer = (req, res) => {
+
+    // Parse the url
+    const parsedUrl = url.parse(req.url, true);
+
+    // Get the path
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Get the query string as an object
+    const queryStringObject = parsedUrl.query;
+
+    // Get the HTTP method
+    const method = req.method.toLowerCase();
+
+    // Get the headers as an object
+    const headers = req.headers;
+
+    // Get the payload, if any
+    const decoder = new StringDecoder('utf8');
+    let buffer = '';
+
+    req.on('data', (data) => {
+        buffer += decoder.write(data);
+    });
+
+    req.on('end', () => {
+        buffer += decoder.end();
+
+        // Check the router for a matching path for a handler.
+        // If one is not found, use the notFound handle instead.
+        const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ?
+            router[trimmedPath] : handlers.notFound;
+
+        // Construct the data object to send to the handler
+        const data = {
+            'trimmedPath': trimmedPath,
+            'queryStringObject': queryStringObject,
+            'method': method,
+            'headers': headers,
+            'payload': helpers.parseJsonToObject(buffer)
+        };
+
+        // Route the request to the handler specified in the router
+        chosenHandler(data, (statusCode, payload) => {
+
+            // Use the status code returned from the handler,
+            // or set the default status code to 200
+            statusCode = typeof(statusCode) === 'number' ? statusCode : 200;
+
+            // Use the payload returned from the handler,
+            // or set the default payload to an empty object
+            payload = typeof(payload) === 'object' ? payload : {};
+
+            // Convert the payload to a string
+            const payloadString = JSON.stringify(payload);
+
+            // Return the response
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            // If the response is 200, print green, otherwise print red
+            if (statusCode === 200) {
+                debug('\x1b[32m%s\x1b[0m', method.toUpperCase() + ' /' + trimmedPath + ' ' + statusCode);
+            } else {
+                debug('\x1b[31m%s\x1b[0m', method.toUpperCase() + ' /' + trimmedPath + ' ' + statusCode);
+            }
+        });
+    });
+};
+
+// Define the request router
+const router = {
+    'ping': handlers.ping,
+    'users': handlers.users,
+    'tokens': handlers.tokens,
+    'checks': handlers.checks
+};
+
+// Init script
+server.init = () => {
+    // Start the HTTP server
+    server.httpServer.listen(config.httpPort, () => {
+        console.log('\x1b[36m%s\x1b[0m', 'The HTTP server is running on port ' + config.httpPort);
+    });
+
+    // Start the HTTPS server
+    server.httpsServer.listen(config.httpsPort, () => {
+        console.log('\x1b[35m%s\x1b[0m', 'The HTTPS server is running on port ' + config.httpsPort);
+    });
+}
+
+// Export the module
+module.exports = server;
